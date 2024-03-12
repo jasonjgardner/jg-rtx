ec59f77ab56176f84965af9322174c8edacbbede
Jason <im@jasongardner.co>
2024-03-12 15:46:00 +0000

    Created Bedrock-only branch

--- src/scripts/atlas_test.ts
+++
@@ -1,9 +0,0 @@
-import {  makeFlipbooks, makeParticles } from './atlas.ts'
-
-Deno.test('Test generating flipbooks', async () => {{
-    await makeFlipbooks()
-}})
-
-Deno.test('Test generating particles', async () => {{
-    await makeParticles()
-}})
