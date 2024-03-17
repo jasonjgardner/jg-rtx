f8ac03a8af5c4f9657e9a9924ff0a507f7857016
Jason Gardner <im@jasongardner.co>
2024-03-17 13:52:35 -0500

    fix(mineways): Corrected paths in textures script

--- mineways/textures.ps1
+++ mineways/textures.ps1
@@ -1,7 +1,17 @@
 # Copy all textures from bedrock\pack\RP and mineways\textures to dist\mineways\blocks
 
-Copy-Item -Path ..\bedrock\pack\RP -Destination ..\dist\mineways\blocks -Recurse -Force
-Copy-Item -Path ..\textures -Destination ..\dist\mineways\blocks -Recurse -Force
+Copy-Item -Path .\bedrock\pack\RP\textures\blocks -Filter *.png -Destination .\dist\mineways\blocks -Recurse -Force
+Copy-Item -Path .\mineways\textures -Destination .\dist\mineways\blocks -Recurse -Force
 
-& "bin\TileMaker\ChannelMixer.exe" -v -m -i bedrock\pack\RP dist\mineways\blocks
-& "bin\TileMaker\TileMaker.exe" -v -m -i bin\TileMaker\terrainBase.png -d dist\mineways\blocks -o dist\mineways\terrainExt_JGRTX256.png -t 256
+$ChannelMixerPath = ".\mineways\bin\mineways\TileMaker\ChannelMixer.exe"
+$TileMakerPath = ".\mineways\bin\mineways\TileMaker\TileMaker.exe"
+
+$cmInput = ".\dist\mineways\blocks"
+$cmOutput = ".\dist\mineways\channelMixer"
+
+if (-not (Test-Path $cmOutput)) {
+    New-Item -Path $cmOutput -ItemType Directory
+}
+
+Start-Process -FilePath $ChannelMixerPath -ArgumentList "-v -i `"$($cmInput)`" -o `"$($cmOutput)`" -m" -Wait -NoNewWindow
+Start-Process -FilePath $TileMakerPath -ArgumentList "-v -m -i .\mineways\bin\mineways\TileMaker\terrainBase.png -d $($cmOutput) -o .\dist\mineways\terrainExt_JGRTX256.png -t 256" -Wait -NoNewWindow
