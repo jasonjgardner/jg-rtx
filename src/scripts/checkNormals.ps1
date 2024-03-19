020bbdfc276ae2d6a2e20d0e9c491a31fc8e5f49
Jason Gardner <im@jasongardner.co>
2024-03-19 10:36:15 -0500

    upd: Check Java & Bedrock normal maps

--- src/scripts/checkNormals.ps1
+++ src/scripts/checkNormals.ps1
@@ -2,12 +2,14 @@ $ntpPath = ".\bin\NormalTextureProcessor.exe"
 
 $ntpDir = ".\dist\ntp"
 
-$src = ".\bedrock\pack\RP\textures\blocks";
+$bedrockSrc = ".\bedrock\pack\RP\textures\blocks\*";
+$javaSrc = ".\java\pack\assets\minecraft\textures\block\*";
 
 if (-not (Test-Path $ntpDir)) {
-    New-Item -Path $ntpDir -ItemType Directory
+    New-Item -Path $ntpDir -ItemType Directory | Out-Null
 }
 
-Copy-Item -Path $src -Filter "*_normal.png" -Destination $ntpDir -Recurse -Force
+Copy-Item -Path $bedrockSrc -Filter "*_normal.png" -Destination $ntpDir -Recurse -Force
+Copy-Item -Path $javaSrc -Filter "*_n.png" -Destination $ntpDir -Recurse -Force
 
-Start-Process -FilePath $ntpPath -ArgumentList "-a -v -idir .\dist\ntp\blocks" -Wait -NoNewWindow -RedirectStandardOutput ".\dist\ntp\ntp.log" -RedirectStandardError ".\dist\ntp\ntp.err"
+Start-Process -FilePath $ntpPath -ArgumentList "-a -v -etol 7 -idir .\dist\ntp" -Wait -NoNewWindow -RedirectStandardOutput ".\dist\ntp\ntp.log" -RedirectStandardError ".\dist\ntp\ntp.err"
